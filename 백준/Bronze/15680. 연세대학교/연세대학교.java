513e924e297db8c5ed1547d01b20259f2f7446d2
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2023-09-09 17:56:38 +0900

    [Bronze V] Title: 연세대학교, Time: 108 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author jihye.byun
+ * BOJ 15680 연세대학교 브론즈5
+ *
+ * 문제
+ *
+ * 조건
+ *
+ * 풀이
+ */
+
+public class Main {
+	
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	if (sc.nextInt() == 0)
+    		System.out.println("YONSEI");
+    	else
+    		System.out.println("Leading the Way to the Future");
+    }
+    
+}
