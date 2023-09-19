f703def10693418d0cd1fbf4249cf8ea53c6e73b
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2023-09-19 19:48:29 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 13132 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,29 +1,16 @@
-import java.util.Scanner;
-
-/**
- * @author jihye.byun
- * BOJ 11021 A+B - 7 브론즈 5
- *
- * 문제
- *
- * 조건
- *
- * 풀이
- */
+import java.util.*;
 
 public class Main {
-	
     public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	
-    	int T = sc.nextInt();
-    	
-    	for (int tc = 1; tc <= T; tc++) {
-    		int a = sc.nextInt();
-    		int b = sc.nextInt();
-    		
-    		System.out.println("Case #" + tc + ": " + (a + b));
-    	}
-    	
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();
+        
+        for (int tc = 1; tc <= T; tc++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            System.out.println("Case #" + tc + ": " + (a + b));
+        }
     }
 }
