29e3b8ee6069d906c359be35ecb610cf1550879e
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2023-10-15 14:37:25 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 120 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt() / 4;
+        
+        for (int idx = 0; idx < N; idx++) {
+            System.out.print("long ");
+        }
+        System.out.println("int");
+    }
+}
