37b39f5d556869b3d8e89d30a1ee8e59f708fa87
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2023-11-01 09:17:46 +0900

    [Silver II] Title: 이동하기, Time: 436 ms, Memory: 78928 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 /**
  * @author jihye.byun
@@ -14,50 +15,30 @@ import java.util.*;
  */
 
 public class Main {
-//	static int[] dr = new int[] {1, 0, 1};
-//	static int[] dc = new int[] {0, 1, 1};
-	
 	static int N, M, maxCandy;
 	static int[][] map;
 	
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
     	
-    	N = sc.nextInt();
-    	M = sc.nextInt();
-    	map = new int[N + 1][M + 1];
+    	N = Integer.parseInt(st.nextToken()); // row 크기
+    	M = Integer.parseInt(st.nextToken()); // col 크기
+    	map = new int[N + 1][M + 1]; // row-1, col-1 에 접근하기 위해 1,1부터 시작하게 map 크기 지정
     	
+    	// 미로에 놓여진 사탕 정보 입력받기
     	for (int row = 1; row <= N; row++) {
+    		st = new StringTokenizer(br.readLine());
     		for (int col = 1; col <= M; col++) {
-    			map[row][col] = sc.nextInt();
-    		}
-    	}
-    	
-//    	maxCandy = 0;
-//    	
-//    	dfs(0, 0, map[0][0]);
-    	
-    	for (int row = 1; row <= N; row++) {
-    		for (int col = 1; col <= M; col++) {
+    			// 현재 칸 사탕 정보를 입력받고
+    			map[row][col] = Integer.parseInt(st.nextToken());
+    			// 왼쪽칸과 윗쪽 칸 중 더 큰 칸을 거쳐오게 하자
+    			// 대각선 왼쪽 윗칸은 왼쪽과 윗쪽칸에서 이미 거쳐왔기 때문에 고려하지 않아도 됨
     			map[row][col] += Math.max(map[row - 1][col], map[row][col - 1]);
     		}
     	}
     	
+    	// 사탕을 제일 많이 주울 수 있는 길로 왔을 때, 마지막 칸에서의 사탕 갯수 출력
     	System.out.println(map[N][M]);
     }
-    
-//    static void dfs(int row, int col, int candy) {
-//    	if (row == N - 1 && col == M - 1) {
-//    		maxCandy = Math.max(maxCandy, candy);
-//    		return;
-//    	}
-//    	
-//    	for (int d = 0; d < 3; d++) {
-//    		int nr = row + dr[d];
-//    		int nc = col + dc[d];
-//    		
-//    		if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-//    		dfs(nr, nc, candy + map[nr][nc]);
-//    	}
-//    }
 }
