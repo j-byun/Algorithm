e274755783a408d6348d8c418f837c864a865fe3
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2023-11-28 22:09:55 +0900

    [Bronze V] Title: 웰컴, Time: 80 ms, Memory: 11356 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author jihye.byun
+ * BOJ 5337 웰컴 브론즈5
+ *
+ * 문제
+ *
+ * 조건
+ *
+ * 풀이
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    	System.out.println(".  .   .");
+    	System.out.println("|  | _ | _. _ ._ _  _");
+    	System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
