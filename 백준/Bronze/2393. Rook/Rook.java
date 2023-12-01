cae82bc732c5a3e0d18f77b36478dab673219509
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2023-12-01 22:23:20 +0900

    [Bronze V] Title: Rook, Time: 72 ms, Memory: 11392 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/**
+ * @author jihye.byun
+ * BOJ 2393 Rook 브론즈5
+ *
+ * 문제
+ *
+ * 조건
+ *
+ * 풀이
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    	System.out.println("  ___  ___  ___");
+    	System.out.println("  | |__| |__| |");
+    	System.out.println("  |           |");
+    	System.out.println("   \\_________/");
+    	System.out.println("    \\_______/");
+    	System.out.println("     |     |");
+    	System.out.println("     |     |");
+    	System.out.println("     |     |");
+    	System.out.println("     |     |");
+    	System.out.println("     |_____|");
+    	System.out.println("  __/       \\__");
+    	System.out.println(" /             \\");
+    	System.out.println("/_______________\\");
+    }
+}
