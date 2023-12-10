ac612f2a7140a362056b68e874bcb4ed45b6f74a
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2023-12-10 16:26:33 +0900

    [Bronze V] Title: Hook, Time: 76 ms, Memory: 11160 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+/**
+ * @author jihye.byun
+ * BOJ 10189 Hook 브론즈5
+ *
+ * 문제
+ *
+ * 조건
+ *
+ * 풀이
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    	System.out.println("#  # #### #### #  #");
+    	System.out.println("#### #  # #  # # #");
+    	System.out.println("#### #  # #  # # #");
+    	System.out.println("#  # #### #### #  #");
+    }
+}
