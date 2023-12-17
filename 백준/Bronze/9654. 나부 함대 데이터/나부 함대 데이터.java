016d7a302a3c65a08a98a3344bc0e9f124af49c6
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2023-12-17 22:00:40 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 68 ms, Memory: 11216 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author jihye.byun
+ * BOJ 9654 나무 함대 데이터 브론즈5
+ *
+ * 문제
+ *
+ * 조건
+ *
+ * 풀이
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    	System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+    	System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
+    	System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
+    	System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
+    	System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");
+    	System.out.println("Royal Cruiser  Light Combat   Limited    4         ");
+    }
+}
