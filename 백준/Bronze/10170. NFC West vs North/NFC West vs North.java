e832b5a9992ee9ef8aa5d32954a33ecd554291d3
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2023-12-20 23:12:36 +0900

    [Bronze V] Title: NFC West vs North, Time: 72 ms, Memory: 11344 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.java
@@ -0,0 +1,29 @@
+
+/**
+ * @author jihye.byun
+ * BOJ 10170 NFC West vs North 브론즈5
+ *
+ * 문제
+ *
+ * 조건
+ *
+ * 풀이
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    	System.out.println("NFC West       W   L  T\n" + 
+    			"-----------------------\n" + 
+    			"Seattle        13  3  0\n" + 
+    			"San Francisco  12  4  0\n" + 
+    			"Arizona        10  6  0\n" + 
+    			"St. Louis      7   9  0\n" + 
+    			"\n" + 
+    			"NFC North      W   L  T\n" + 
+    			"-----------------------\n" + 
+    			"Green Bay      8   7  1\n" + 
+    			"Chicago        8   8  0\n" + 
+    			"Detroit        7   9  0\n" + 
+    			"Minnesota      5  10  1");
+    }
+}
