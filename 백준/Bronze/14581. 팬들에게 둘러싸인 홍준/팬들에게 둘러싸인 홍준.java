76b5d8b0ed4655fe0ccd98acdc8b826070251492
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2023-12-27 23:04:30 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 104 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/**
+ * @author jihye.byun
+ * BOJ 14581 팬들에게 둘러싸인 홍준 브론즈5
+ *
+ * 문제
+ *
+ * 조건
+ *
+ * 풀이
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	String name = sc.next();
+    	
+    	System.out.println(":fan::fan::fan:");
+    	System.out.println(":fan::" + name + "::fan:");
+    	System.out.println(":fan::fan::fan:");
+    }
+}
