c6faecc930800c4ca49837ee4e1ce410ff77d197
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2024-01-27 11:15:32 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11284 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author jihye.byun
+ * BOJ 25083 새싹 브론즈5
+ *
+ * 문제
+ *
+ * 조건
+ *
+ * 풀이
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    	System.out.println("         ,r'\"7");
+    	System.out.println("r`-_   ,'  ,/");
+    	System.out.println(" \\. \". L_r'");
+    	System.out.println("   `~\\/");
+    	System.out.println("      |");
+    	System.out.println("      |");
+    }
+}
