2562dedbcac75a8a692901a2b1c830e69b01dd16
Jihye Byun <122416904+j-byun@users.noreply.github.com>
2025-02-17 00:34:08 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 96 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        for (int cnt = 1; cnt <= N; cnt++) {
+            System.out.print("LoveisKoreaUniversity ");
+        }
+    }
+}
